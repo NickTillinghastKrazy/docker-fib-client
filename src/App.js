83fe4122d4c43b694f72e1b61f5ff1fec1adfcce
Nick Tillinghast <nick.tillinghast@thekrazycouponlady.com>
2021-12-22 14:47:57 -0700

    client file with docker image and fib page and routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
 import './App.css';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-title">Welcome to React</h1>
+        <Link to="/">Home</Link>
+        <Link to="/otherpage">Other Page</Link>
       </header>
+      <div>
+        <Route exact path="/" component={Fib}/>
+        <Route path="/otherpage" component={OtherPage}/>
+      </div>
     </div>
+    </Router>
   );
 }
 
